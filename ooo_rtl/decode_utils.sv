e52078517f6da22868169cc2e4b650c75ae1fc71
Kunal <kpaode@gmail.com>
2026-03-28 12:48:46 +0000

    worked on connecting wires in decode stage

--- ooo_rtl/decode_utils.sv
+++ ooo_rtl/decode_utils.sv
@@ -40,6 +40,42 @@ package issue_queue_pkg;
         logic [$clog2(PRF_COUNT)-1:0] src1_ptr;
     } iq_output_t;
 
+    // SUBJECT TO CHANGE
+    function automatic int get_exec_stage_delays (
+        input [INSTR_COMPRESS_WIDTH-1:0] op;
+    );
+        return 3;
+    endfunction
+
+    // SUBJECT TO CHANGE
+    function automatic int get_exec_stage_delays_from_instr (
+        input [31:0] instr;
+    );
+        return 3;
+    endfunction
+
+    // sets some entries to be 0, will be overwritten later
+    function automatic iq_output_t instr_to_iq_entry_partial (
+        input [31:0] instr;
+    );
+        iq_output_t iq_entry;
+        iq_entry.valid = 0;
+        iq_entry.op = grab_compr_instr(instr);
+        iq_entry.exec_dur = get_exec_stage_delays_from_instr(instr);
+        iq_entry.imm_valid = decode_pkg::has_imm(instr[6:0]);
+        iq_entry.imm_compr = extract_20b_imm(instr);
+        iq_entry.speculative = decode_pkg::is_speculative(instr[6:0]);
+        iq_entry.dest_valid = decode_pkg::has_dest(instr[6:0]);
+        iq_entry.dest_ptr = 0;
+        iq_entry.src0_valid = decode_pkg::has_src0(instr[6:0]);
+        iq_entry.src0_pending = 0;
+        iq_entry.src0_ptr = 0;
+        iq_entry.src1_valid = decode_pkg::has_src1(instr[6:0]);
+        iq_entry.src1_pending = 0;
+        iq_entry.src1_ptr = 0;
+        return iq_entry;
+    endfunction
+
     // SUBJECT TO CHANGE, CAN OPTIMIZE
     // "-3" b/c we are removing some valid and pending bits
     // localparam INSTR_OUTPUT_WIDTH = $bits(iq_entry_t) - 3;
@@ -61,6 +97,7 @@ package issue_queue_pkg;
         return out;
     endfunction
 
+    // could set some bits to zero based on type of instruction type
     function automatic logic [INSTR_COMPRESS_WIDTH-1:0] grab_compr_instr (
         input [31:0] full_instr
     );
@@ -76,8 +113,8 @@ package issue_queue_pkg;
 
     // endfunction
 
-    import general_pkg::instruction_t;
-    import general_pkg::classify_instr;
+    import decode_pkg::instruction_t;
+    import decode_pkg::classify_instr;
 
     // UNSURE IF THIS CORRECT
     function automatic imm_gen_32 (
@@ -114,6 +151,69 @@ package issue_queue_pkg;
         endcase
         return immediate;
     endfunction
+
+    // disclaimer: function below has been ai generated
+    function automatic logic [19:0] extract_20b_imm(input logic [31:0] instr);
+    logic [6:0] opcode;
+    opcode = instr[6:0];
+
+    case (opcode)
+        // ----------------------------------------------------------------
+        // I-Type: 12 bits (sign-extended to 20)
+        // ----------------------------------------------------------------
+        7'b000_0011, 7'b110_0111, 7'b001_0011:
+        begin
+            extract_20b_imm = {{8{instr[31]}}, instr[31:20]};
+        end
+
+        // ----------------------------------------------------------------
+        // S-Type: 12 bits (sign-extended to 20)
+        // ----------------------------------------------------------------
+        7'b010_0011:
+        begin
+            extract_20b_imm = {{8{instr[31]}}, instr[31:25], instr[11:7]};
+        end
+
+        // ----------------------------------------------------------------
+        // B-Type: 13-bit value (12 encoded + 1 implicit 0, sign-extended to 20)
+        // ----------------------------------------------------------------
+        7'b110_0011:
+        begin
+            extract_20b_imm = {{7{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+        end
+
+        // ----------------------------------------------------------------
+        // U-Type: 20 bits (Exact fit)
+        // ----------------------------------------------------------------
+        7'b011_0111, 7'b001_0111:
+        begin
+            extract_20b_imm = instr[31:12];
+        end
+
+        // ----------------------------------------------------------------
+        // J-Type: 20 raw encoded bits (Excludes the implicit 0)
+        // ----------------------------------------------------------------
+        7'b110_1111:
+        begin
+            // A JAL immediate is technically a 21-bit offset. To fit it into 
+            // a 20-bit signal without losing the sign bit, we omit the implicit LSB 0.
+            extract_20b_imm = {instr[31], instr[19:12], instr[20], instr[30:21]};
+        end
+
+        // ----------------------------------------------------------------
+        // Z-Type (CSR): 5 bits (zero-extended to 20)
+        // ----------------------------------------------------------------
+        7'b111_0011:
+        begin
+            extract_20b_imm = {15'b0, instr[19:15]};
+        end
+
+        // Default: No immediate
+        default: begin
+            extract_20b_imm = 20'b0;
+        end
+    endcase
+endfunction
 
 
 
