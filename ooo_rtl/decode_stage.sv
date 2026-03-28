df0c86ccb08b9db838cb030a3447912c0c727ffb
Kunal <kpaode@gmail.com>
2026-03-28 04:12:28 +0000

    added mini_freelist, aswell as began connecting modules in decode stage

--- ooo_rtl/decode_stage.sv
+++ ooo_rtl/decode_stage.sv
@@ -1,10 +1,13 @@
-module decode_stage #(
+module decode_stage 
+    import general_pkg::*;
+#(
     parameter DATA_WIDTH = 32,
-    parameter INSTR_WIDTH = 32,
-
 ) (
     input clk,
-    input [INSTR_WIDTH-1:0] instr_i,
+    input rst,
+    input flush_i,
+    input [31:0] instr_i,
+    output error_o,
     // cntrls
 
     /*
@@ -17,19 +20,195 @@ module decode_stage #(
                 commit_stage w for RT
     */
 
-    output logic
+    // free list
+    input free_list_wr_en_i,
+    input [PRF_COUNT-1:0] free_list_commited_ptr_i, // from scoreboard
+    output logic free_list_empty_o;
+
+    // rename table
+    // to signal its in PRF, can have the scoreboard tell the RT
+    // actually i think it should coe frommright before 
+    input logic [$clog2(PRF_COUNT)-1:0] rename_table_prf_ptr_i,
+    input logic [4:0] rename_table_arf_ptr_sb_i,
+    input logic rename_table_wb_en_i,
+
+    // issue queue
+    // signals for updating state
+    input logic [$clog2(PRF_COUNT)-1:0] issue_queue_prf_dst_i,
+    input logic issue_queue_prf_wr_en_i,
+    
+    // output logic
+    
 );
     // addr for branch targets
 
     // internal comms
     // rename table r/w
     // issue queue w
-    
-    // modules
-    // rename table
-    // issue queue
 
+    /* free list */
+    logic free_list_rd_en;
+    logic [PRF_COUNT-1:0] free_list_free_ptr;
+    // logic free_list_empty;
+
+    free_list free_list_inst #(
+        parameter PRF_COUNT
+    ) (
+        .clk(clk),
+        .rst(rst),
+        // writing
+        .wr_en_i(free_list_wr_en_i),
+        .commited_ptr_i(free_list_commited_ptr_i),
+        // reading
+        .rd_en_i(free_list_rd_en),
+        .free_ptr_o(free_list_free_ptr),
+        // state
+        .empty_o(free_list_empty_o)
+    );
+
+    /* rename table */
+    // intial write
+    logic [$clog2(PRF_COUNT)-1:0] rename_table_prf_dest_ptr;
+    logic [4:0] rename_table_arf_ptr;
+    logic rename_table_decode_en;
+    // reading
+    logic [4:0] rename_table_arf_src0;
+    logic [4:0] rename_table_arf_src1;
+    logic [$clog2(PRF_COUNT)-1:0] rename_table_prf_src0;
+    logic [$clog2(PRF_COUNT)-1:0] rename_table_prf_src1;
+    logic rename_table_src0_pending;
+    logic rename_table_src1_pending;
+
+    rename_table_async_read rename_table_async_read_inst #(
+        parameter PRF_COUNT = 32
+    ) (
+        .clk(clk),
+        .rst(rst),
+        // initial write
+        .prf_ptr_i(rename_table_prf_dest_ptr),
+        .arf_ptr_i(rename_table_arf_ptr),
+        .decode_en_i(rename_table_decode_en),
+        // to signal its in PRF, can have the scoreboard tell the RT
+        .prf_ptr_sb_i(rename_table_prf_ptr_i),
+        .arf_ptr_sb_i(rename_table_arf_ptr_sb_i),
+        .writeback_en_i(rename_table_wb_en_i),
+        // ports for issue queue to read from 
+        .arf_src0_i(rename_table_arf_src0),
+        .arf_src1_i(rename_table_arf_src1),
+        .prf_src0_o(rename_table_prf_src0),
+        .prf_src1_o(rename_table_prf_src1),
+        .src0_pending_o(rename_table_src0_pending),
+        .src1_pending_o(rename_table_src1_pending)
+    );
+
+    /* issue queue */
+    iq_entry_t issue_queue_entry;
+    iq_output_t issue_queue_instr_out;
+    logic issue_queue_empty;
+    logic issue_queue_full;
+    logic issue_queue_all_stalled;
+    logic [MAX_EXEC_CYCLE:0] issue_queue_future_exec_stage_slots;
+
+    issue_queue issue_queue_inst (
+        .clk(clk),
+        .rst(rst),
+        .iq_entry_i(issue_queue_entry),
+        // updating state of pending bit
+        // for writing from right before writeback stage into decode stage
+        .prf_dst_i(issue_queue_prf_dst_i),
+        .prf_wr_en_i(issue_queue_prf_wr_en_i),
+        // for checking for structural hazards
+        .future_exec_stage_slots_i(issue_queue_future_exec_stage_slots),      
+        .instr_o(issue_queue_instr_out),
+        // also other output information 
+        .empty_o(issue_queue_empty),
+        .full_o(issue_queue_full),
+        .all_stalled_o(issue_queue_all_stalled) // when all current entries are still stalling, does also account for input "prf_dst_i"
+    );
+
+    logic mini_scoreboard_wr_en;
+    logic [INSTR_COMPRESS_WIDTH-1:0] mini_scoreboard_op;
+    logic mini_scoreboard_clear;
+
+    mini_scoreboard mini_scoreboard_inst (
+        .clk(clk),
+        .rst(rst),
+        // new entry
+        .wr_en_i(mini_scoreboard_wr_en),
+        .op_i(mini_scoreboard_op),
+        // outputting to iq
+        // 1 extra: 1 for reg fetch stage
+        .future_exec_stage_slots_o(issue_queue_future_exec_stage_slots),
+        // stalling or clearing
+        .clear_i(mini_scoreboard_clear), // same functionality as rst
+    );
+
+    // idk what this in reference to
     // keep track of next avail rob ptr val (increment by 1 using an adder each time we get an instruciton that writes to a register)
+
+
+
+    // setting cntrl instructions
+    logic dispatch_instr;
+    logic new_instr_ready; // all operands are avail
+    always_comb begin
+        
+        free_list_rd_en = 1;
+
+        // determined by operands being ready
+        // free list not being empty, assuming it needs it
+        new_instr_ready = 
+            !(has_dest(instr_i[6:0]) && free_list_empty) ||
+            !(has_src0(instr_i[6:0]) && rename_table_src0_pending) ||
+            !(has_src1(instr_i[6:0]) && rename_table_src1_pending);
+        
+        dispatch_instr = 
+            new_instr_ready || 
+            (!issue_queue_empty && !issue_queue_all_stalled);
+
+        if (!dispatch_instr) begin
+            free_list_rd_en = 0;
+
+        end
+
+    end
+
+    // checking for errors
+    always_comb begin
+        if ()
+    end
+
+    /* decoding a new instruction as an input to issue queue */
+    always_comb begin
+        issue_queue_entry = '0;
+        if (!stall) begin
+            issue_queue_entry.valid = 1;
+            issue_queue_entry.op = grab_compr_instr(instr);
+            imm_valid = has_imm(instr[6:0]);
+            imm = imm_gen(instr);
+            speculative = is_speculative(instr[6:0]);
+            dest_valid = has_dest(instr[6:0]);
+            dest_ptr = free_list[]
+
+        end
+    end
+    // op = 
+    // access issue queue
+
+
+
+
+
+    // routing an instruction
+
+
+    /* 
+        interms of output of decoder (actually decoding things) we need to 
+        take a iq_output_t as a input
+
+    */
+
+
     
 
 endmodule
@@ -96,12 +275,21 @@ module rename_table_async_read #(
     input logic [4:0] arf_ptr_i,
     input logic decode_en_i, 
     // to signal its in PRF, can have the scoreboard tell the RT
+    // note: actually prob wont be scoreboard, 
+    // is instead wil berigt before writeback
+    // another reason for not being sb is b/c if an interrupt happenss
+    // want to have 
     input logic [$clog2(PRF_COUNT)-1:0] prf_ptr_sb_i,
     input logic [4:0] arf_ptr_sb_i,
     input logic writeback_en_i,
     // ports for issue queue to read from 
-    output logic [$clog2(PRF_COUNT)-1:0] prf_ptr_o
-);
+    input logic [4:0] arf_src0_i,
+    input logic [4:0] arf_src1_i,
+    output logic [$clog2(PRF_COUNT)-1:0] prf_src0_o,
+    output logic [$clog2(PRF_COUNT)-1:0] prf_src1_o,
+    output logic src0_pending_o,
+    output logic src1_pending_o
+);0
     typedef struct packed {
         logic [$clog2(PRF_COUNT)-1:0] prf_ptr;
         // logic [DATA_WIDTH-1:0] imm;
@@ -131,7 +319,11 @@ module rename_table_async_read #(
     end
     
     // read logic (asynchronous)
-    assign prf_ptr_o = rename_table[arf_ptr_i].rob_ptr;
+    assign prf_src0_o = rename_table[arf_src0_i].rob_ptr;
+    assign src0_pending_o = rename_table[arf_src0_i].pending;
+    assign prf_src1_o = rename_table[arf_src1_i].rob_ptr;
+    assign src1_pending_o = rename_table[arf_src1_i].pending;
+
 endmodule
 
 // should be the module that determines which is the next instruction to be issued
@@ -140,31 +332,33 @@ module issue_queue
     (
     input clk,
     input rst,
-    // could optimize, but would have to move some of the write logic outside module
-    input logic [31:0] instr_i,
-    input logic src_
-    input logic [$clog2(PRF_COUNT)-1:0] src0_ptr,
-    input logic [$clog2(PRF_COUNT)-1:0] src1_ptr,
-    input logic [$clog2(PRF_COUNT)-1:0] dst_ptr,
-    input logic wr_en_i,
+
+    input iq_entry_t iq_entry_i,
+
     // signal from exterior
 
     // updating state of pending bit
     // for writing from right before writeback stage into decode stage
     input logic [$clog2(PRF_COUNT)-1:0] prf_dst_i,
     input logic prf_wr_en_i,
+    // for checking for structural hazards
+    // 1 extra: 1 for reg fetch stage
+    input logic [MAX_EXEC_CYCLE:0] future_exec_stage_slots_i,
+    
     
     // output logic [INSTR_COMPRESS_WIDTH-1:0] compr_instr_o;
-    output logic [INSTR_OUTPUT_WIDTH-1:0] instr_o;
+    output iq_output_t instr_o;
     // also other output information 
     output logic empty_o,
     output logic full_o,
-    output logic all_stalled_o, // when all current entries are still stalling, does also account for input "rob_dst_i"
+    output logic all_stalled_o, // when all current entries are still stalling, does also account for input "prf_dst_i"
 );  
 
     iq_entry_t iq [0:IQ_SIZE-1];
 
     // write logic
+    logic wr_en;
+    assign wr_en = iq_entry_i.valid;
     always_ff @(posedge clk) begin
         if (rst) begin
             iq <= '0;
@@ -174,21 +368,20 @@ module issue_queue
             all_stalled_o <= 0;
         end 
         else begin
-            if (wr_en_i && !full_o) begin
+            if (wr_en && !full_o) begin
                 // find first empty slot and write instruction
                 for (int i = 0; i < IQ_SIZE; i++) begin
                     if (!iq[i].valid) begin
                         iq[i].valid <= 1;
-                        iq[i].op <= grab_compr_instr(instr);
-                        iq[i].imm <= imm_gen(instr);
-                        iq[i].speculative <= is_speculative(instr[6:0]);
-                        iq[i].dest_valid <= is_dest(instr[6:0]);
-                        iq[i].dest_ptr <= dest_ptr;
-                        iq[i].src0_valid <= has_src0(instr[6:0]);
-                        // iq[i].pe
-
-                            
-                        end
+                        iq[i][$bits(iq_entry_t)-2:0] <= iq_entry_i[$bits(iq_entry_t)-2:0];
+                        // iq[i].valid <= 1;    
+                        // iq[i].op <= grab_compr_instr(instr);
+                        // iq[i].imm <= imm_gen(instr); // dont use
+                        // iq[i].speculative <= is_speculative(instr[6:0]);
+                        // iq[i].dest_valid <= is_dest(instr[6:0]);
+                        // iq[i].dest_ptr <= dest_ptr;
+                        // iq[i].src0_valid <= has_src0(instr[6:0]);
+                        // // iq[i].pe
                         break;
                     end
                 end
@@ -196,8 +389,18 @@ module issue_queue
         end
     end
 
+    // updating pending state
+    always_ff @(posedge clk) begin
+        for (int i = 0; i < IQ_SIZE; i++) begin
+            if (iq[i].src0_valid && iq[i].src0_pending && iq[i].src0_ptr == prf_dst_i)
+                iq[i].src0_pending = 0;
+            if (iq[i].src1_valid && iq[i].src1_pending && iq[i].src1_ptr == prf_dst_i)
+                iq[i].src1_pending = 0;
+        end
+    end
+
     // read logic
-    // also can account for forwarding from before even reaching commit stage, but would haav eot check conditions before
+    // also can account for forwarding from before even reaching commit stage, but would have to check conditions before
     logic [IQ_SIZE-1:0] valid_array;
     always_comb begin
         for (int i = 0; i < IQ_SIZE; i++)
@@ -207,43 +410,89 @@ module issue_queue
     assign full_o = &valid_array;
 
     logic [IQ_SIZE-1:0] ready_array;
+    // expensive, see if can optimize
     always_comb begin
         for (int i = 0; i < IQ_SIZE; i++) begin
             ready_array[i] = (
-                (!iq[i].src0_valid | (iq[i].src0_pending & (iq[i].src0_ptr == rob_dst_i))) & 
-                (!iq[i].src1_valid | (iq[i].src1_pending & (iq[i].src1_ptr == rob_dst_i)))
+                (!iq[i].src0_valid || !iq[i].src0_pending || iq[i].src0_ptr == prf_dst_i) &&
+                (!iq[i].src1_valid || !iq[i].src1_pending || iq[i].src1_ptr == prf_dst_i) &&
+                (future_exec_stage_slots_i[iq[i].exec_dur] == 0)
             );
         end
+        all_stalled_o = ~(|ready_array);
     end
-    assign all_stalled_o = ~(|ready_array);
 
-    always_ff @(posedge clk) begin
-
+    // asynch read
+    logic [IQ_SIZE-1:0] priority_ready_array;
+    always_comb begin
+        priority_ready_array = '0;
+        instr_o = '0; 
         if (!empty && !all_stalled_o) begin
-            // search for highest priority valid entry
+            // search for highest priority "ready" entry
             for (int i = 0; i < IQ_SIZE; i++) begin
-                if (iq[i].valid) begin
-                    // for now, we will output almost the entire entry (MAKE CHANGE LATER)
-
+                if (ready_array[i]) begin
+                    priority_ready_array[i] = 1;
+                    instr_o = entry_to_output(iq[i]);
+                    break;
                 end
             end
         end
-
-        // probably delete this:
-        instr_o = '0;
-        for (int i = 0; i < IQ_SIZE; i++) begin
-            // adj forr SRC!!!
-            if (iq[i].pending == 1'b0) begin : grab_from_rob
-                instr_o = iq[i].instr;
-                // send rob ptrs to grab from rob
-                rob_ptr = 
-                // send dest out
-                break;
-            end else if (iq valid = ) begin : grab_from_arf
-
+    end
+    // synchronously updating validity
+    always_ff @(posedge clk) begin
+        if (!empty && !all_stalled_o) begin
+            for (int i = 0; i < IQ_SIZE; i++) begin
+                if (priority_ready_array[i]) begin
+                    iq[i].valid = 0;
+                end
             end
         end
     end
+endmodule
+
+
+module mini_scoreboard
+import issue_queue_pkg::*;
+(
+    input clk,
+    input rst,
+    // new entry
+    input wr_en_i,
+    input [INSTR_COMPRESS_WIDTH-1:0] op_i,
+    // outputting to iq
+    // 1 extra: 1 for reg fetch stage
+    output logic [MAX_EXEC_CYCLE:0] future_exec_stage_slots_o,
+    // stalling or clearing
+    input clear_i, // same functionality as rst
+);
+    // SUBJECT TO CHANGE
+    function int get_exec_stage_delays (
+        input [INSTR_COMPRESS_WIDTH-1:0] op;
+    );
+        return 3;
+    endfunction
+
+    // 2 extra: 1 for reg fetch stage, 1 for wb to follow design from slides
+    logic [MAX_EXEC_CYCLE+1:0] exec_stage_slots_int;
+
+    always_ff @(posedge clk) begin
+        if (rst || clear_i) begin
+            exec_stage_slots_int <= '0;
+        end else begin
+            for (int i = MAX_EXEC_CYCLE+1; i >= 0; i--) begin
+                if (i == MAX_EXEC_CYCLE+1) begin
+                    exec_stage_slots_int[i] <= 0;
+                end else begin
+                    exec_stage_slots_int[i] <= exec_stage_slots_int[i+1];
+                end
+            end
+            if (wr_en_i) begin
+                exec_stage_slots_int[get_exec_stage_delays(op_i)] <= 1;
+            end
+        end
+    end
+
+    assign exec_stage_slots_o = exec_stage_slots_int[MAX_EXEC_CYCLE+1:1];
 
 endmodule
 
--- ooo_rtl/decode_utils.sv
+++ ooo_rtl/decode_utils.sv
@@ -1,23 +1,20 @@
 package issue_queue_pkg;
 
-    // replacing instruction with rob ptr
-    typedef struct packed {
-        logic [31:0] instr;
-        logic []
-    } rob_instr;
-
-
     // localparam DATA_WIDTH = 32;
     localparam IQ_SIZE = 16;
     // localparam ROB_COUNT = 32;
     localparam PRF_COUNT = 32;
     localparam INSTR_COMPRESS_WIDTH = 17;
+    localparam MAX_EXEC_CYCLE = 4;
+    localparam IMM_COMPRESS = 20;
 
-    // could optimize by diverging from diaram
+
     typedef struct packed {
         logic valid;
         logic [INSTR_COMPRESS_WIDTH-1:0] op;
-        logic [DATA_WIDTH-1:0] imm;
+        logic [$clog2(MAX_EXEC_CYCLE)-1:0] exec_dur;
+        logic imm_valid;
+        logic [IMM_COMPRESS-1:0] imm_compr;
         logic speculative;
         logic dest_valid;
         logic [$clog2(PRF_COUNT)-1:0] dest_ptr;
@@ -28,10 +25,41 @@ package issue_queue_pkg;
         logic src1_pending;
         logic [$clog2(PRF_COUNT)-1:0] src1_ptr;
     } iq_entry_t;
+    
+    typedef struct packed {
+        logic [INSTR_COMPRESS_WIDTH-1:0] op;
+        logic [$clog2(MAX_EXEC_CYCLE)-1:0] exec_dur;
+        logic imm_valid;
+        logic [IMM_COMPRESS-1:0] imm_compr;
+        logic speculative;
+        logic dest_valid;
+        logic [$clog2(PRF_COUNT)-1:0] dest_ptr;
+        logic src0_valid;
+        logic [$clog2(PRF_COUNT)-1:0] src0_ptr;
+        logic src1_valid;
+        logic [$clog2(PRF_COUNT)-1:0] src1_ptr;
+    } iq_output_t;
 
     // SUBJECT TO CHANGE, CAN OPTIMIZE
-    // "-5" b/c we are removing valid and pending bits
-    localparam INSTR_OUTPUT_WIDTH = $bits(iq_entry_t) - 5;
+    // "-3" b/c we are removing some valid and pending bits
+    // localparam INSTR_OUTPUT_WIDTH = $bits(iq_entry_t) - 3;
+    function automatic iq_output_t entry_to_output (
+        input iq_entry_t in
+    );
+        iq_output_t out;
+        out.op = in.op;
+        out.exec_dur    = in.exec_dur;
+        out.imm_valid   = in.imm_valid;
+        out.imm_compr   = in.imm_compr;
+        out.speculative = in.speculative;
+        out.dest_valid  = in.dest_valid;
+        out.dest_ptr    = in.dest_ptr;
+        out.src0_valid  = in.src0_valid;
+        out.src0_ptr    = in.src0_ptr;
+        out.src1_valid  = in.src1_valid;
+        out.src1_ptr    = in.src1_ptr;
+        return out;
+    endfunction
 
     function automatic logic [INSTR_COMPRESS_WIDTH-1:0] grab_compr_instr (
         input [31:0] full_instr
@@ -51,12 +79,12 @@ package issue_queue_pkg;
     import general_pkg::instruction_t;
     import general_pkg::classify_instr;
 
-    function automatic imm_gen #(
-        parameter DATA_WIDTH = 32
-    ) (
+    // UNSURE IF THIS CORRECT
+    function automatic imm_gen_32 (
         input  logic [31:0] instruction,
-        output logic [DATA_WIDTH-1:0] immediate
     );
+        logic [6:0] opcode;
+        assign opcode = instruciton[6:0];
         // Internal logic for immediate generation
         case (opcode)
             // I-type (ADDI, SLTI, JALR, LW, etc.)
@@ -84,7 +112,7 @@ package issue_queue_pkg;
                 immediate = 32'b0;
             end
         endcase
-        return opcode;
+        return immediate;
     endfunction
 
 
