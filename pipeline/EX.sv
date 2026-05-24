d7292d25aedaf2cfd4d83408555e00dcebb82ac7
Kunal <kunalpaode@gmail.com>
2026-05-24 05:18:06 +0000

    deleted old rtl folders and files

--- pipeline/EX.sv
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-
-module EX #(parameter WIDTH=32)( 
-    );
-    // 5 stage pipeline
-    
-endmodule
--- pipeline/MEM.sv
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-
-module MEM #(parameter WIDTH=32)( 
-    );
-    // 5 stage pipeline
-    
-endmodule
